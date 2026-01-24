da9e8dfc4861ab1179efa06d0d07315b74d6e9f5
Anirudh <anirudhnush@gmail.com>
2026-01-25 00:53:27 +0800

    Add greeting

---
+++ src/main/java/Ani.java
@@ -0,0 +1,42 @@
+public class Duke {
+    public static void main(String[] args) {
+
+        String logo = "    ___     ___   ___  _______ \n"
+                +
+                "   /   \\   |   \\  | | |__  __|\n"
+                    + "  / /_\\ \\  | |\\ \\ | |   |  |\n"
+                    + " / ----- \\ | | \\ \\| | __|  |__        \n"
+                    + "/__/   \\__\\| |  \\___| |______|           ";
+
+
+        /*
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+         */
+        System.out.println("Hello from\n" + logo);
+
+        String intro = "___________________________________\n" +
+                "Hello! I'm Ani\n"
+                + "What can I do for you?";
+
+        String exit = "___________________________________\n" +
+                "Bye. Hope to see you again soon!\n" + "___________________________________";
+
+
+
+        System.out.println(intro);
+        System.out.println(exit);
+
+
+
+
+
+
+
+
+    }
+}
